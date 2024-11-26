ea6c3bf5768bc6df8c2308dea05f37a251556372
bttglc <bottiglierigianluca@icloud.com>
2024-11-26 11:47:40 +0100

    update main

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [todos, setTodos] = useState([]);
+
+  const handleAddTodo = (newTask) => {
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: Date.now(), task: newTask, completed: false },
+    ]);
+  };
+
+  const handleRemoveTodo = (id) => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  };
+
+  const handleUpdateTodo = (id, newTask, completed = null) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === id
+          ? {
+              ...todo,
+              task: newTask !== undefined ? newTask : todo.task,
+              completed: completed !== null ? completed : todo.completed,
+            }
+          : todo
+      )
+    );
+  };
 
   return (
-    <>
+    <div className="app">
+      <h1 className="app-title">Dodo</h1>
+      <TodoForm onAddTodo={handleAddTodo} />
+      <TodoList
+        todos={todos}
+        onRemoveTodo={handleRemoveTodo}
+        onUpdateTodo={handleUpdateTodo}
+      />
+    </div>
+  );
+};
 
-    </>
-  )
-}
-
-export default App
+export default App;
